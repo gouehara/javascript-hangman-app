fd6e7579dd92c93f3046d505e8d206f84f26adb8
gouehara <ueharanaha@gmail.com>
2019-11-16 22:55:02 +0900

    update.

--- app.js
+++ app.js
@@ -12,32 +12,40 @@ window.addEventListener('keypress', function (e) {
       guessesEl.textContent = game1.statusMessage
 })
 
-const request = new XMLHttpRequest();
-
-request.addEventListener('readystatechange', (e) => {
-      if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText);
-            console.log(data);
-      } else if (e.target.readyState === 4) {
-            console.log('an error has taken place');
+getPuzzle((error,puzzle) => {
+      if (error) {
+            console.log(`Error: ${error}`);
+      } else {
+            console.log(puzzle)
       }
 });
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+// const request = new XMLHttpRequest();
 
-const countryCode = 'JP';
-const countryRequest = new XMLHttpRequest();
+// request.addEventListener('readystatechange', (e) => {
+//       if (e.target.readyState === 4 && e.target.status === 200) {
+//             const data = JSON.parse(e.target.responseText);
+//             console.log(data);
+//       } else if (e.target.readyState === 4) {
+//             console.log('an error has taken place');
+//       }
+// });
 
-countryRequest.addEventListener('readystatechange', e => {
-      if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText)
-            const country = data.find(country => country.alpha2Code === countryCode)
-            console.log(country.name);
-      } else if (e.target.responseText === 4) {
-            console.log('an error has taken place');
-      }
-})
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+// request.send()
 
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-countryRequest.send()
+// const countryCode = 'JP';
+// const countryRequest = new XMLHttpRequest();
+
+// countryRequest.addEventListener('readystatechange', e => {
+//       if (e.target.readyState === 4 && e.target.status === 200) {
+//             const data = JSON.parse(e.target.responseText)
+//             const country = data.find(country => country.alpha2Code === countryCode)
+//             console.log(country.name);
+//       } else if (e.target.responseText === 4) {
+//             console.log('an error has taken place');
+//       }
+// })
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
