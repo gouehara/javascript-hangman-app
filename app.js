ee5238722983be9cea7bce9f31dd97b15a286bd4
gouehara <ueharanaha@gmail.com>
2019-11-17 15:46:56 +0900

    update.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener('keypress', function (e) {
       guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle((error,puzzle) => {
+getPuzzle(1 ,(error,puzzle) => {
       if (error) {
             console.log(`Error: ${error}`);
       } else {
@@ -20,32 +20,10 @@ getPuzzle((error,puzzle) => {
       }
 });
 
-// const request = new XMLHttpRequest();
-
-// request.addEventListener('readystatechange', (e) => {
-//       if (e.target.readyState === 4 && e.target.status === 200) {
-//             const data = JSON.parse(e.target.responseText);
-//             console.log(data);
-//       } else if (e.target.readyState === 4) {
-//             console.log('an error has taken place');
-//       }
-// });
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-// request.send()
-
-// const countryCode = 'JP';
-// const countryRequest = new XMLHttpRequest();
-
-// countryRequest.addEventListener('readystatechange', e => {
-//       if (e.target.readyState === 4 && e.target.status === 200) {
-//             const data = JSON.parse(e.target.responseText)
-//             const country = data.find(country => country.alpha2Code === countryCode)
-//             console.log(country.name);
-//       } else if (e.target.responseText === 4) {
-//             console.log('an error has taken place');
-//       }
-// })
-
-// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-// countryRequest.send()
+getCountry('JP', (error, country) => {
+      if (error) {
+            console.log(error)
+      } else {
+            console.log(country.name)
+      }
+});
