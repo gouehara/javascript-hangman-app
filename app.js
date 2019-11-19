3c9e988f07f07f34e40cd5596875b8e877f9c794
gouehara <ueharanaha@gmail.com>
2019-11-19 22:50:46 +0900

    update.

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cat food', 2)
-
-puzzleEl.textContent = game1.puzzle
-guessesEl.textContent = game1.statusMessage
-
+let game1
 window.addEventListener('keypress', function (e) {
       const guess = String.fromCharCode(e.charCode)
       game1.makeGuess(guess)
-      puzzleEl.textContent = game1.puzzle
-      guessesEl.textContent = game1.statusMessage
+      render();
 })
 
-getPuzzle(1 ,(error,puzzle) => {
-      if (error) {
-            console.log(`Error: ${error}`);
-      } else {
-            console.log(puzzle)
-      }
-});
+const render = () => {
+      puzzleEl.textContent = game1.puzzle
+      guessesEl.textContent = game1.statusMessage
+};
 
-getCountry('JP', (error, country) => {
-      if (error) {
-            console.log(error)
-      } else {
-            console.log(country.name)
-      }
-});
+const startGame = async () => {
+      const puzzle = await getPuzzle(2);
+      game1 = new Hangman(puzzle, 5)
+      render();
+};
+
+document.querySelector('#reset').addEventListener('click', startGame);
+
+startGame();
+
+getPuzzle(5).then(data=> {
+      console.log(data);
+}).catch(error => {
+      console.log(error);
+})
+
+// getCountry('JP').then(data => {
+//       console.log(data.currencies[0].symbol);
+// }).catch(error => {
+//       console.log(error);
+// })
+
+// getCurrentCountry().then(data => {
+//       console.log(data);
+// })
+
+// getLocation().then(data => data.country
+// ).then(data => getCountry(data)
+// ).then(data => {
+//       console.log(data.name);
+// }).catch(error => {
+//       console.log(error)
+// })
+
+// fetch('http://puzzle.mead.io/puzzle?wordCount', {}).then(response => {
+//       if (response.status === 200) {
+//             return response.json();
+//       } else {
+//             throw new Error('unable to fetch the puzzle')
+//       }
+// }).then(data => {
+//       console.log(data.puzzle);
+// }).catch(error => {
+//       console.log(error);
+// })
