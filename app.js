3ad54734544b73d11ca32790ae85a49834ca7ecd
gouehara <ueharanaha@gmail.com>
2019-11-19 22:59:04 +0900

    update.

--- app.js
+++ app.js
@@ -1,59 +1,36 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 let game1
-window.addEventListener('keypress', function (e) {
+
+window.addEventListener('keypress', (e) => {
       const guess = String.fromCharCode(e.charCode)
       game1.makeGuess(guess)
-      render();
+      render()
 })
 
 const render = () => {
       puzzleEl.textContent = game1.puzzle
       guessesEl.textContent = game1.statusMessage
-};
+}
 
 const startGame = async () => {
-      const puzzle = await getPuzzle(2);
+      const puzzle = await getPuzzle('2')
       game1 = new Hangman(puzzle, 5)
-      render();
-};
+      render()
+}
 
-document.querySelector('#reset').addEventListener('click', startGame);
+document.querySelector('#reset').addEventListener('click', startGame)
 
-startGame();
+startGame()
 
-getPuzzle(5).then(data=> {
-      console.log(data);
-}).catch(error => {
-      console.log(error);
-})
-
-// getCountry('JP').then(data => {
-//       console.log(data.currencies[0].symbol);
-// }).catch(error => {
-//       console.log(error);
+// getPuzzle('2').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
 // })
 
-// getCurrentCountry().then(data => {
-//       console.log(data);
-// })
-
-// getLocation().then(data => data.country
-// ).then(data => getCountry(data)
-// ).then(data => {
-//       console.log(data.name);
-// }).catch(error => {
-//       console.log(error)
-// })
-
-// fetch('http://puzzle.mead.io/puzzle?wordCount', {}).then(response => {
-//       if (response.status === 200) {
-//             return response.json();
-//       } else {
-//             throw new Error('unable to fetch the puzzle')
-//       }
-// }).then(data => {
-//       console.log(data.puzzle);
-// }).catch(error => {
-//       console.log(error);
+// getCurrentCountry().then((country) => {
+//     console.log(country.name)
+// }).catch((error) => {
+//     console.log(error)
 // })
