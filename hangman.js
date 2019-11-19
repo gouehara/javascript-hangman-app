eeee8bf213c7301652ec7bb45950df38b0102eb4
gouehara <ueharanaha@gmail.com>
2019-11-19 23:02:03 +0900

    update.

--- hangman.js
+++ hangman.js
@@ -22,7 +22,7 @@ class Hangman {
             } else if (this.status === 'failed') {
                   return `Nice try! The word was "${this.word.join('')}".`
             } else {
-                  return 'Great work! You guessed the work.'
+                  return 'Great work! You guessed the word.'
             }
       }
       get puzzle() {
