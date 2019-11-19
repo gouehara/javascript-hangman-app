a82877e536a11c19cec017afc5f2873674c8d7fe
gouehara <ueharanaha@gmail.com>
2019-11-19 23:00:42 +0900

    update.

--- requests.js
+++ requests.js
@@ -9,28 +9,28 @@ const getPuzzle = async (wordCount) => {
       }
 }
 
-// const getCurrentCountry = async () => {
-//       const location = await getLocation()
-//       return getCountry(location.country)
-// }
+const getCurrentCountry = async () => {
+      const location = await getLocation()
+      return getCountry(location.country)
+}
 
-// const getCountry = async (countryCode) => {
-//       const response = await fetch('http://restcountries.eu/rest/v2/all')
+const getCountry = async (countryCode) => {
+      const response = await fetch('http://restcountries.eu/rest/v2/all')
 
-//       if (response.status === 200) {
-//             const data = await response.json()
-//             return data.find((country) => country.alpha2Code === countryCode)
-//       } else {
-//             throw new Error('Unable to fetch the country')
-//       }
-// }
+      if (response.status === 200) {
+            const data = await response.json()
+            return data.find((country) => country.alpha2Code === countryCode)
+      } else {
+            throw new Error('Unable to fetch the country')
+      }
+}
 
-// const getLocation = async () => {
-//       const response = await fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c')
+const getLocation = async () => {
+      const response = await fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c')
 
-//       if (response.status === 200) {
-//             return response.json()
-//       } else {
-//             throw new Error('Unable to get the current location')
-//       }
-// }
+      if (response.status === 200) {
+            return response.json()
+      } else {
+            throw new Error('Unable to get the current location')
+      }
+}
